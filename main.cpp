223df1ac4406b89233cca87a75fd549767178e18
druzhynets <amsterdancer@gmail.com>
2018-10-07 12:26:15 +0200

    Added pseudo-code for the new algorithm

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
+#if 0
 #include "mpcc.h"
+#endif
 
 int main()
 {
-
-
+#if 0
     MPCC votes("https://en.numista.com/forum/topic73312.html");
     votes.get_voting_results();
+#endif
 
 
-    return EXIT_SUCCESS;
+
 }
--- uncroppedimagesfinder.cpp
+++ uncroppedimagesfinder.cpp
@@ -2,5 +2,14 @@
 
 UncroppedImagesFinder::UncroppedImagesFinder()
 {
-
+    // 1. Get name of the country/issuer which has to be investigated
+    // 2. Download the list of the countries webpage
+    // 3. Extract the URL to the given country
+    // 4. Download the country webpage
+    // 5. Extract the number of the pages
+    // 6. If more then one page, repeat 7 for each page
+    // 7. Get list of entries, append to main list of entries
+    // 8. For each entry, get images (max 2)
+    // 9. If at least one of the images is uncropped, add entrie's url to the final list
+    // 10. Output the final list
 }
